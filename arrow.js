5c32177b9248ef1724570bb57ff176f1f9d63e5d
Rabiulrn <rnrabiul@gmail.com>
2021-08-30 15:48:37 +0600

    arrow fn update

--- arrow.js
+++ arrow.js
@@ -26,3 +26,29 @@ const result = add(50, 70);
 const result2 = give5();
 const result3 = doMath(7, 5);
 console.log(result3);
+
+
+//new
+// function declaration
+function add(num1, num2) {
+    return num1 + num2;
+}
+// function expression 
+const add2 = function add2(num1, num2) {
+    return num1 + num2;
+}
+// function expression (anonymous)
+const add3 = function (num1, num2) {
+    return num1 + num2;
+}
+//arrow function
+const add4 = (num1, num2) => num1 + num2;
+
+const sum1 = add(15, 17);
+const sum2 = add2(15, 17);
+const sum3 = add3(15, 17);
+const sum4 = add4(15, 17);
+console.log(sum1, sum2, sum3, sum4);
+// document.getElementById('my-btn').onclick = function handleEvent(){
+
+// }
