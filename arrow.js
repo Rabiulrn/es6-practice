7790287551d3d6f8baf2d3c21b074ced01bcea88
Rabiulrn <rnrabiul@gmail.com>
2021-08-30 16:27:33 +0600

    spread updated

--- arrow.js
+++ arrow.js
@@ -51,4 +51,33 @@ const sum4 = add4(15, 17);
 console.log(sum1, sum2, sum3, sum4);
 // document.getElementById('my-btn').onclick = function handleEvent(){
 
-// }
+// 
+
+
+
+const add = (num1, num2) => num1 + num2;
+const sum = add(22, 90);
+
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(4, 12, 3);
+
+const tenTimes = (num) => num * 10;
+const output = tenTimes(17);
+
+const fiveTimes = num => num * 5;
+const value = fiveTimes(17);
+
+const getName = () => 'Brandon Sam';
+const name = getName()
+console.log(name);
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    const output = result * 5;
+    return output;
+}
+
+const total = doMath(12, 5);
+console.log(total);
--- threedots.js
+++ threedots.js
@@ -15,3 +15,20 @@ const takaPoisa = [650, 450, 250, 850];//array akare thakle
 // const maximum = Math.max(business, minister, sochib);
 const maximum = Math.max(...takaPoisa);//... dile sereally bose jabe
 console.log(maximum);
+
+
+//new
+
+const numbers = [23, 65, 99, 21, 34];
+// console.log(numbers);
+// console.log(...numbers);
+
+const max = Math.max(23, 99, 65, 21, 34);
+const maxInArray = Math.max(...numbers);            //max er vitore array dile kaj kore na.tai ... diye val;u gulo niye aste hoy******
+// console.log(max, maxInArray);
+
+const numbers2 = [...numbers, 88];                  //aivabe dile numbers2 alada jayga nibe & alada array hisebe kaj korbe
+// const numbers2 = numbers   //aivabe dile ,numbers a push korle numbers2 teo dekhabe(reference er jonno)
+numbers.push(55);
+console.log(numbers);
+console.log(numbers2);
